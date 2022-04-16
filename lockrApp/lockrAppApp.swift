b8f81567f808c1eb95f50aa31c1ed4a5f46f0121
Rahul Chander <rahul.chander02@gmail.com>
2022-04-15 23:14:07 -0700

    Initial Commit

---
+++ lockrApp/lockrAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  lockrAppApp.swift
+//  lockrApp
+//
+//  Created by Rahul Chander on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct lockrAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
