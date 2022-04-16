4d2f035546155c86793d93d4e912158f96e0f07a
Rahul Chander <rahul.chander02@gmail.com>
2022-04-16 08:57:55 -0700

    simple fixes

---
+++ lockrApp/Views/lockrPage.swift
@@ -0,0 +1,24 @@
+//
+//  lockrPage.swift
+//  lockrApp
+//
+//  Created by Rahul Chander on 4/16/22.
+//
+
+import SwiftUI
+
+struct lockrPage: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+        
+    }
+}
+
+struct lockrPage_Previews: PreviewProvider {
+    static var previews: some View {
+        lockrPage()
+    }
+}
