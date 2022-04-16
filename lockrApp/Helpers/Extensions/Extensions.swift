11446c563b67d41c8b1e0d6471ec61c8bbccc75f
Rahul Chander <rahul.chander02@gmail.com>
2022-04-15 23:50:47 -0700

    extensions

---
+++ lockrApp/Helpers/Extensions/Extensions.swift
@@ -0,0 +1,30 @@
+//
+//  Extensions.swift
+//  lockrApp
+//
+//  Created by Rahul Chander on 4/15/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    struct myTheme {
+        
+        static var PurpleColor: Color {
+            return Color("ColorPurple")
+        }
+        
+        static var GreenColor: Color {
+            return Color("ColorGreen")
+        }
+        
+        static var BeigeColor: Color {
+            return Color("ColorBeige")
+        }
+        
+    }
+    
+}
+
