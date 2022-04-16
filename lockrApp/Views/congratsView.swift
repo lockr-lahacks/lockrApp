3417cb6adcf016dcb9356c4bc75f44b39c21de4b
Alan Yu <44562288+111alanyu@users.noreply.github.com>
2022-04-16 14:07:05 -0700

    Current status of the congrats screen

--- lockrApp/Views/congratsView.swift
+++ lockrApp/Views/congratsView.swift
@@ -9,17 +9,29 @@ import SwiftUI
 
 struct congratsView: View {
     var body: some View {
-        VStack {
+        
+        ZStack {
+            
             Color.myTheme.GreenColor
                 .ignoresSafeArea()
-            Rectangle()
-                .fill(Color.white)
-                .frame(width: 350, height: 400)
-                .cornerRadius(15)
-            Text("Congratulations!").font(.custom("Inter Semi Bold", size: 30)).multilineTextAlignment(.center)
-            //You have successfully retu...
-            Text("You have successfully returned the item to the lockr ").font(.custom("Inter Medium", size: 16)).foregroundColor(Color(#colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1))).multilineTextAlignment(.center)
-
+           
+                Rectangle()
+                    .fill(Color.white)
+                    .cornerRadius(15)
+                    .frame(width: 375, height: 200)
+                    .foregroundColor(Color(#colorLiteral(red: 0.4, green: 0.4, blue: 0.4, alpha: 1))).multilineTextAlignment(.center)
+            VStack{
+                Text("Congratulations!").font(.custom("Inter Semi Bold", size: 30)).multilineTextAlignment(.center)
+                    .padding(5)
+                //You have successfully retu...
+                Text("You have successfully returned the item to the lockr ").font(.custom("Inter Medium", size: 16))
+                    .frame(width: 350)
+                Rectangle()
+                    .fill(Color.blue)
+                    .cornerRadius(10)
+                    .frame(width: 200, height: 50)
+                    
+            }
         }
         
         
