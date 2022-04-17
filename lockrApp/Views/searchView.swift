5385b55f37b6035fabaaf724a2e99e8583eba59c
Alan Yu <44562288+111alanyu@users.noreply.github.com>
2022-04-16 18:22:30 -0700

    Basic Search set up done

---
+++ lockrApp/Views/searchView.swift
@@ -0,0 +1,42 @@
+//
+//  searchView.swift
+//  lockrApp
+//
+//  Created by Alan Yu on 4/16/22.
+//
+
+import SwiftUI
+
+struct searchView: View {
+    @State private var searchText = ""
+    let names = ["Test", "Apple", "Go"]
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(searchResults, id: \.self) { name in
+                    NavigationLink(destination: Text(name)) {
+                        Text(name)
+                    }
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Items Near By")
+        }
+
+    }
+    
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return names
+        } else {
+            return names.filter { $0.contains(searchText) }
+        }
+    }
+}
+
+struct searchView_Previews: PreviewProvider {
+    static var previews: some View {
+        searchView()
+    }
+}
+
